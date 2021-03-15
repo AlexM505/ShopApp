9ff38b0313c973c09f222b2a1cbf80cb6ad2222e
cmarenco <cmarenco@thedataage.com>
2021-03-15 14:45:59 -0600

    Agregando la orden

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/cart.dart' show Cart;
+import '../providers/orders.dart';
 import '../widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget{
@@ -33,7 +34,10 @@ class CartScreen extends StatelessWidget{
                     backgroundColor: Theme.of(context).primaryColor,
                   ),
                   TextButton(
-                    onPressed: (){}, 
+                    onPressed: (){
+                      Provider.of<Orders>(context, listen: false).addOrder(cart.items.values.toList(), cart.totalAmount);
+                      cart.clear();
+                    }, 
                     child: Text('ORDER NOW'),
                     style: TextButton.styleFrom(primary: Theme.of(context).primaryColor)
                   )
